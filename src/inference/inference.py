73273b04918dcac6b23faa9b102858ff1403b824
Luke Meyers <functor.soup@gmail.com>
2025-04-26 18:53:36 -0400

    Use scan rather a loop. The JAX way

--- src/inference/inference.py
+++ src/inference/inference.py
@@ -1,33 +1,48 @@
+import equinox as eqx
 import jax
-import jax.random as random
 import jax.numpy as jnp
+import jax.random as random
+from jaxtyping import Array, Float, Integer
 
 
-def simple_inference(diffusion_model, key, timesteps, noise_shape, output_as_perturbation=True):
+def simple_inference(
+    diffusion_model: eqx.Module,
+    key: jax.PRNGKeyArray,
+    timesteps: int,
+    noise_shape: Integer[Array, "..."],
+    output_as_perturbation: bool = True,
+) -> Float[Array, "batch timesteps channel spatial_width spatial_width "]:
     """
 
-     
+    Utter simple reverse sampling algorithm
+
+
 
 
     """
 
-    keys_ = jax.random.split(key, timesteps + 1)
+    key, data_key = jax.random.split(key, 2)
 
-    x = jax.random.normal(keys_[0], noise_shape)  # 1000,
+    x = random.normal(data_key, noise_shape)  # 1000,
 
-    steps = [x]
+    v_reverse_diffusion = jax.vmap(diffusion_model, in_axes=(None, 0))
 
-    v_reverse_diffusion = jax.vmap(diffusion_model, in_axes=(None,0))
+    def f(acc, t):
 
-    for t in reversed(range(timesteps)):
-        mu, sigma = v_reverse_diffusion(t,x)
-        # split key here
+        key, x = acc
+
+        acc_key, key = jax.random.split(key, 2)
+
+        mu, sigma = v_reverse_diffusion(t, x)
+
         if output_as_perturbation:
-            x += mu + random.normal(keys_[t + 1], noise_shape) * sigma
+            # For the spiral dataset
+            x += mu + random.normal(key, noise_shape) * sigma
         else:
-            x = mu + random.normal(keys_[t + 1], noise_shape) * sigma
-        steps.append(x)
+            x = mu + random.normal(key, noise_shape) * sigma
 
-    print([x.shape for x in steps])
+        return (acc_key, x), x
 
-    return jnp.stack(steps)  # T, B, 2 perhaps we should change this?
+    _, result = jax.lax.scan(f, (key, x), jnp.arange(timesteps - 1, -1, -1))
+
+    return result
